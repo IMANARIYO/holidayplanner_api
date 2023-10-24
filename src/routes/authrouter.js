7e3f15c3a0ab3d0c7d0b1644875ab0cd505aa8d7
IMANARIYO <imanariyobaptiste@gmail.com>
2023-10-24 14:08:40 +0200

    unlocalising

--- src/routes/authrouter.js
+++ src/routes/authrouter.js
@@ -4,14 +4,15 @@ import { deleteUser, getAllUsers } from "../controllers/trip";
 import { signup,login,changepassword }from "../controllers/authentication/index.js";
 import { verifyingtoken } from "../middleware/index.js";
 const authRouter=express.Router();
+
 authRouter.post("/signup", signup);
 authRouter.post("/login", login);
-// authRouter.use(verifyingtoken)
-// authRouter.get("/", getAllUsers);
 
-authRouter.post("/changepassword",verifyingtoken, changepassword);
-//authRouter.post("/isadmin", verifyingtoken,isAdmin);
-// authRouter.delete("/deleteuser/:id",isAdmin,deleteUser);
+authRouter.use(verifyingtoken)
 authRouter.get("/", getAllUsers);
-authRouter.delete("/deleteuser/:id",deleteUser);
+authRouter.post("/changepassword",verifyingtoken, changepassword);
+authRouter.post("/isadmin", verifyingtoken,isAdmin);
+authRouter.delete("/deleteuser/:id",isAdmin,deleteUser);
+// authRouter.get("/", getAllUsers);
+// authRouter.delete("/deleteuser/:id",deleteUser);
 export default authRouter;
