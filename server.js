45714528e1cf134e267e881cce6432b153192635
IMANARIYO <imanariyobaptiste@gmail.com>
2023-10-29 21:17:30 +0200

    testing for general errors handling  methods

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import mainRouter from "./src/routes/index.js";
 import swaggerUi from "swagger-ui-express";
-import { badroutes } from "./src/middleware/index.js";
+import { badroutes, errosingeneral } from "./src/middleware/index.js";
 const app = express();
 
 const swaggerDocument = yaml.load("./yamlfile.yaml");
@@ -18,10 +18,9 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use("/", mainRouter);
 app.all('*',badroutes)
+app.use(errosingeneral);
 
-
-mongoose
-  .connect(process.env.DB_CONNECTION_LIVE, {
+mongoose.connect(process.env.DB_CONNECTION_LIVE, {
     useNewUrlParser: true,
     useUnifiedTopology: true
   })
--- src/controllers/trip/add.js
+++ src/controllers/trip/add.js
@@ -7,6 +7,7 @@ import {
 import uploadCloudinary from "../../utils/cloudinary";
 import { contactHtmlMessage } from "../authentication/index.js";
 import { sendEmail } from "../../utils";
+import {catchAsync} from"../../middleware/index.js"
 import dotenv from "dotenv";
 import { v2 as cloudinary } from "cloudinary";
 dotenv.config();
@@ -16,59 +17,53 @@ cloudinary.config({
   api_secret: process.env.API_SECRET
 });
 
-const insertOneDynamic = model => {
-  return async (req, res) => {
-    try {
-let tourId=req.tourId;
-let userId=req.userId,userEmail=req.userEmail
+const insertOneDynamic = model =>
+ {    
+    return async (req, res,next) =>
+   {
+    
+       let tourId=req.tourId;
+       let userId=req.userId,userEmail=req.userEmail
       let newObject = { ...req.body,userId,userEmail };
       
 
-      if (req.files && req.files["image"]) {
-        newObject.image = (await cloudinary.uploader.upload(
-          req.files["image"][0].path
-        )).secure_url;
+      if (req.files && req.files["image"])
+       {
+                newObject.image = (await cloudinary.uploader.upload(
+                  req.files["image"][0].path
+                )).secure_url;
       }
 
-      if (req.files && req.files.gallery) {
-        let imagesArray = [];
-        for (let index = 0; index < req.files.gallery.length; index++) {
-          imagesArray.push(
-            (await cloudinary.uploader.upload(req.files.gallery[index].path))
-              .secure_url
-          );
+      if (req.files && req.files.gallery)
+       {
+           console.log("in gallery")
+           let imagesArray = [];
+           for (let index = 0; index < req.files.gallery.length; index++)
+            {imagesArray.push((await cloudinary.uploader.upload(req.files.gallery[index].path)) .secure_url); }
+           newObject.gallery = imagesArray;
         }
-        newObject.gallery = imagesArray;
-      }
 
       let data = await model.create(newObject);
       //checking if the request is contact as it has the message field
-      if(req.body.message){
-await sendEmail(userEmail, "your contact message have been received", "thank yoou for contacting us!", contactHtmlMessage);
-console.log(req.body.message)
-console.log(userEmail)
+      if(req.body.message)
+      {
+        await sendEmail(userEmail, "your contact message have been received", "thank yoou for contacting us!", contactHtmlMessage);
+        console.log(req.body.message)
+        console.log(userEmail)
       }
 
-      if (!data) {
-        return res
+      if (!data)
+       {
+          return res
           .status(404)
           .json({ message: `${model.modelName} failed to add` });
       }
 
-      return res.status(200).json({
-        message: `${model.modelName} created successfully`,
-        data: data
-      });
-    } catch (err) {
-      console.log(`Error adding ${model.modelName}`, err);
-      return res.status(500).json({
-        message: `There was an error adding the ${model.modelName}.`,
-        error: err.message
-      });
+      return res.status(200).json({message: `${model.modelName} created successfully`,
+        data: data });
     }
-  };
-};
-export const insertTour = insertOneDynamic(tourconst);
-export const insertContact = insertOneDynamic(contactconst);
-export const insertTestimony = insertOneDynamic(testmonyconst);
-export const insertBooking = insertOneDynamic(bookingconst);
+  }
+export const insertTour = catchAsync(insertOneDynamic(tourconst));
+export const insertContact = catchAsync(insertOneDynamic(contactconst));
+export const insertTestimony = catchAsync(insertOneDynamic(testmonyconst));
+export const insertBooking = catchAsync(insertOneDynamic(bookingconst));
